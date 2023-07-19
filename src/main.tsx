d2e7e0f84f23fd98a4009a551085537f3a148586
AndreeaHr <andreeahirceaga24@gmail.com>
2023-07-19 20:00:13 +0300

    Validate the root

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+const root = document.querySelector<HTMLElement>('#root')
+
+if(!root) {
+  throw new Error('Root not found')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
