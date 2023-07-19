ef7eef78dba43fc3cbb3af3b55ab0f57b2c29ec0
AndreeaHr <andreeahirceaga24@gmail.com>
2023-07-19 21:27:20 +0300

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,127 @@ function App() {
 
   return (
     <>
-      
+      <header>
+        <div>
+          <img src="" alt="" />
+          <a href=""><img src="" alt="" /></a>
+        </div>
+
+        <div>
+          <a href=""><img src="" alt="" /></a>
+          <a href=""><img src="" alt="" /></a>
+        </div>
+        <img src="" alt="" />
+
+        <input type="text" />
+
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Favorit</a></li>
+            <li><a href="">Cart</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <section>
+        <h1 className="text-red-500">Alege cartea ta</h1>
+        <div>
+          <a href=""><img src="" alt="" /></a>
+          <a href=""><img src="" alt="" /></a>
+          <a href=""><img src="" alt="" /></a>
+          <a href=""><img src="" alt="" /></a>
+        </div>
+      </section>
+
+      <section>
+        <article>
+          <h1>Science Fiction</h1>
+          <div>
+            <div>
+              <a href=""><img src="" alt="" /></a>
+              <div>
+                <h3>Pierce Brown</h3>
+                <h3>Furia Rosie</h3>
+                <h4>20,65 lei</h4>
+              </div>
+            </div>
+
+            <div>
+              <a href=""><img src="" alt="" /></a>
+              <div>
+                <h3>Philip Reeve</h3>
+                <h3>Masinarii infernale</h3>
+                <h4>56,32 lei</h4>
+              </div>
+            </div>
+          </div>
+        </article>
+
+        <article>
+          <h1>Romance</h1>
+          <div>
+            <div>
+              <a href=""><img src="" alt="" /></a>
+              <div>
+                <h3>Martin Lugand</h3>
+                <h3>Oameni fericiti citesc si beau cafea</h3>
+                <h4>34,55 lei</h4>
+              </div>
+            </div>
+            <div>
+              <a href=""><img src="" alt="" /></a>
+              <div>
+                <h3>Alexander Dumas </h3>
+                <h3>dama cu camelii</h3>
+                <h4>45,7 lei</h4>
+              </div>
+            </div>
+          </div>
+        </article>
+
+        <article>
+          <h1>Personal development</h1>
+          <div>
+            <div>
+              <a href=""><img src="" alt="" /></a>
+              <div>
+                <h3>Marcus Aurelius</h3>
+                <h3>Ganduri catre sine insusi</h3>
+                <h4>45.33 lei</h4>
+              </div>
+            </div>
+            <div>
+              <a href=""><img src="" alt="" /></a>
+              <div>
+                <h3>F.M.Dostoievski</h3>
+                <h3>Fratii Karamazov</h3>
+                <h4>56,89 lei</h4>
+              </div>
+            </div>
+          </div>
+        </article>
+
+
+      </section>
+
+      <footer>
+        <div>
+          <h3></h3>
+          <a href=""><img src="" alt="" /></a>
+        </div>
+
+        <div>
+          <h3></h3>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </div>
+      </footer>
+
+
+
+
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "./index.css"
 
 const root = document.querySelector<HTMLElement>('#root')
 
-if(!root) {
+if (!root) {
   throw new Error('Root not found')
 }
 
